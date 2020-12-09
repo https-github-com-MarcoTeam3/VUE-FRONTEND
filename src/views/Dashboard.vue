fb56cd623bff9ee4b80a2d026588cfd588e8c780
Slambert <sharif.lewis-lambert@epitch.eu>
2020-12-09 16:57:18 +0100

    removed commentaire

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,12 +8,6 @@
     </nav>
     <div class="container">
       <h1>Dashboard</h1>
-      <!-- <div class="card">
-        <div class="card-body" v-if="user">
-          <h3>Hello, {{ user.name }}</h3>
-          <span>{{ user.email }}</span>
-        </div>
-      </div> -->
     </div>
     <v-container id="user-profile" fluid tag="section">
       <v-row justify="center">
