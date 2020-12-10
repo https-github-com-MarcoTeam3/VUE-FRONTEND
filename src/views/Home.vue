fa5434874b79b9b2fa6a9067913fc9c3e0448fe1
Slambert <sharif.lewis-lambert@epitch.eu>
2020-12-10 15:05:53 +0100

    added component Brands to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,52 +34,7 @@
         More than <strong class="red--text">50</strong> brands in stock
       </h2>
     </v-container>
-    <v-container class="pt-12">
-      <v-row>
-        <v-col cols="4" sm="2">
-          <v-img
-            src="https://d2yac1vwzyjed0.cloudfront.net/assets/front/catalog/brands_logo/brand_101-8cca61e9947991b93447da23685a1487.png"
-          ></v-img>
-        </v-col>
-
-        <v-col cols="2" sm="2">
-          <v-img
-            src="https://i.postimg.cc/RCsy1dV8/585e9f11cb11b227491c3502.png"
-          >
-            <div class="fill-height bottom-gradient"></div>
-          </v-img>
-        </v-col>
-
-        <v-col cols="2" sm="2">
-          <v-img
-            src="https://i.postimg.cc/XqLCFXbS/58429f65a6515b1e0ad75aed.png"
-          >
-            <div class="fill-height repeating-gradient"></div>
-          </v-img>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <v-img
-            src="https://i.postimg.cc/pT2VjCXr/5a1ac46ef65d84088faf132c.png"
-          >
-            <div class="fill-height repeating-gradient"></div>
-          </v-img>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <v-img
-            src="https://d2yac1vwzyjed0.cloudfront.net/assets/front/catalog/brands_logo/brand_129-cdbaa6a05e2ccfe9974c75a1c60f200a.png"
-          >
-            <div class="fill-height repeating-gradient"></div>
-          </v-img>
-        </v-col>
-        <v-col cols="2" sm="2">
-          <v-img
-            src="https://i.postimg.cc/dQh3jWjZ/5a1ac93bf65d84088faf137c.png"
-          >
-            <div class="fill-height repeating-gradient"></div>
-          </v-img>
-        </v-col>
-      </v-row>
-    </v-container>
+    <Brands></Brands>
     <v-container>
       <v-row no-gutters>
         <v-col :cols="12">
@@ -189,14 +144,16 @@
 </template>
 
 <script>
+import Brands from "@/components/Brands.vue"
 import Footer from "@/components/Footer.vue";
 import { VueTyper } from "vue-typer";
 
 export default {
   components: {
-    Footer,
-    "vue-typer": VueTyper,
+    Brands, Footer,
+    "vue-typer": VueTyper
   },
+   name: "Home",
   data() {
     return {
       items: [
@@ -219,7 +176,7 @@ export default {
       ],
     };
   },
-  name: "Home",
+ 
 };
 </script>
 
