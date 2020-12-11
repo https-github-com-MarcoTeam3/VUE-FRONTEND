a0e3542153c3589f3aabd34886dd546dabad8867
Mukul Gargani <gargani@Manmohans-MacBook-Pro.local>
2020-12-11 11:07:34 +0100

    copyrights text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,18 +14,6 @@
         </div>
 
         <div class="col-xs-6 col-md-3">
-          <h6>Categories</h6>
-          <ul class="footer-links">
-            <li><a href="#">C</a></li>
-            <li><a href="#">UI Design</a></li>
-            <li><a href="#">PHP</a></li>
-            <li><a href="#">Java</a></li>
-            <li><a href="#">Android</a></li>
-            <li><a href="#">Templates</a></li>
-          </ul>
-        </div>
-
-        <div class="col-xs-6 col-md-3">
           <h6>Quick Links</h6>
           <ul class="footer-links">
             <li><a href="#">About Us</a></li>
@@ -48,8 +36,8 @@
       <div class="row">
         <div class="col-md-8 col-sm-6 col-xs-12">
           <p class="copyright-text">
-            Copyright &copy; 2017 All Rights Reserved by
-            <a href="#">Scanfcode</a>.
+            Copyright &copy; 2020 All Rights Reserved by
+            <a href="#">SGM INC</a>.
           </p>
         </div>
         <div class="col-md-4 col-sm-6 col-xs-12">
